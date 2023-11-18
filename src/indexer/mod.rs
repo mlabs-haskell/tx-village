a40b5a66da6c363e1cfe424b1b955ef1dcf6c9d2
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2023-11-18 11:58:06 -0700

    Implement full event handler

--- src/indexer/mod.rs
+++ src/indexer/mod.rs
@@ -1,6 +1,8 @@
 use std::default::Default;
 use std::fmt::Debug;
+use std::ops::Mul;
 use std::sync::Arc;
+use std::time::Duration;
 
 use anyhow::Result;
 use oura::{
@@ -67,11 +69,17 @@ pub struct IndexerConfig<E: Debug + ErrorPolicyProvider> {
   safe_block_depth: usize,
   event_filter: SelectionConfig,
   /// Callback function to pass events to
-  callback_fn: fn(Event) -> Result<(), E>,
+  callback_fn: fn(&Event) -> Result<(), E>,
+  /// Retry policy - how much to retry for each event callback failure
+  /// This only takes effect on ErrorPolicy for a particular error is `Retry`.
+  /// Once retries are exhausted, the handler will error (same treatment as ErrorPolicy::Exit)
+  retry_policy: RetryPolicy,
 }
 
 // This is based on: https://github.com/txpipe/oura/blob/27fb7e876471b713841d96e292ede40101b151d7/src/bin/oura/daemon.rs
-pub fn run_indexer<E: Debug + ErrorPolicyProvider>(conf: IndexerConfig<E>) -> Result<(), Error> {
+pub fn run_indexer<E: Debug + ErrorPolicyProvider + 'static>(
+  conf: IndexerConfig<E>,
+) -> Result<(), Error> {
   let chain = match conf.network_magic {
     NetworkMagic::PREPROD => ChainWellKnownInfo::preprod(),
     NetworkMagic::PREVIEW => ChainWellKnownInfo::preview(),
@@ -115,6 +123,7 @@ pub fn run_indexer<E: Debug + ErrorPolicyProvider>(conf: IndexerConfig<E>) -> Re
 
   let sink_handle = Callback {
     f: conf.callback_fn,
+    retry_policy: conf.retry_policy,
     utils,
   }
   .bootstrap(filter_rx)?;
@@ -128,14 +137,41 @@ pub fn run_indexer<E: Debug + ErrorPolicyProvider>(conf: IndexerConfig<E>) -> Re
 }
 
 // TODO(chase): Implement custom callback based sink
-struct Callback<E: Debug + ErrorPolicyProvider> {
-  f: fn(Event) -> Result<(), E>,
+struct Callback<E> {
+  f: fn(&Event) -> Result<(), E>,
+  retry_policy: RetryPolicy,
   utils: Arc<Utils>,
 }
 
-impl<E: Debug + ErrorPolicyProvider> SinkProvider for Callback<E> {
+impl<E: Debug + ErrorPolicyProvider + 'static> SinkProvider for Callback<E> {
   fn bootstrap(&self, input: StageReceiver) -> BootstrapResult {
-    todo!("Implement custom sink")
+    let callback_fn = self.f.clone();
+    let retry_policy = self.retry_policy;
+    let utils = self.utils.clone();
+    let handle = std::thread::spawn(move || {
+      handle_event(input, callback_fn, &retry_policy, utils).expect("request loop failed")
+    });
+
+    Ok(handle)
+  }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct RetryPolicy {
+  pub max_retries: u32,
+  pub backoff_unit: Duration,
+  pub backoff_factor: u32,
+  pub max_backoff: Duration,
+}
+
+impl Default for RetryPolicy {
+  fn default() -> Self {
+    Self {
+      max_retries: 20,
+      backoff_unit: Duration::from_millis(5_000),
+      backoff_factor: 2,
+      max_backoff: Duration::from_millis(20 * 5_000),
+    }
   }
 }
 
@@ -150,5 +186,64 @@ pub trait ErrorPolicyProvider
 where
   Self: Sized,
 {
-  fn get_error_policy(self) -> ErrorPolicy<Self>;
+  fn get_error_policy(&self) -> ErrorPolicy<Self>;
+}
+
+fn compute_backoff_delay(policy: &RetryPolicy, retry: u32) -> Duration {
+  let units = policy.backoff_factor.pow(retry);
+  let backoff = policy.backoff_unit.mul(units);
+  core::cmp::min(backoff, policy.max_backoff)
+}
+
+fn handle_event<E: Debug + ErrorPolicyProvider>(
+  input: StageReceiver,
+  callback_fn: impl Fn(&Event) -> Result<(), E>,
+  retry_policy: &RetryPolicy,
+  utils: Arc<Utils>,
+) -> Result<(), E> {
+  for event in input.iter() {
+    let mut retry = 0;
+
+    // The retry logic is based on: https://github.com/txpipe/oura/blob/27fb7e876471b713841d96e292ede40101b151d7/src/utils/retry.rs
+    loop {
+      let result = callback_fn(&event);
+
+      match result {
+        Ok(_) => {
+          // notify progress to the pipeline
+          utils.track_sink_progress(&event);
+          break;
+        }
+        Err(err) => match err.get_error_policy() {
+          ErrorPolicy::Exit => return Err(err),
+          ErrorPolicy::Skip => {
+            log::warn!("failed to send webhook request: {:?}", err);
+            break;
+          }
+          ErrorPolicy::Call(err_f) => {
+            err_f(err);
+            break;
+          }
+          ErrorPolicy::Retry if retry < retry_policy.max_retries => {
+            log::warn!("retryable operation error: {:?}", err);
+
+            retry += 1;
+
+            let backoff = compute_backoff_delay(&retry_policy, retry);
+
+            log::debug!(
+              "backoff for {}s until next retry #{}",
+              backoff.as_secs(),
+              retry
+            );
+
+            std::thread::sleep(backoff);
+          }
+          _ => return Err(err),
+        },
+      }
+    }
+  }
+
+  Ok(())
 }
