e8fbaa7a99ca4f01ac096dc7fdb04c79b38d9e71
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2023-11-19 14:46:20 -0700

    Initial handler impl

--- src/handler.rs
+++ src/handler.rs
@@ -1,17 +1,34 @@
-use oura::model::Event;
+use oura::model::{Event, EventData};
 
 use sqlx::PgConnection;
 
-use infinity_query::indexer::error::{ErrorPolicy, ErrorPolicyProvider};
+use infinity_query::{
+  database::{TransactionDbError, TransactionSql},
+  indexer::error::{ErrorPolicy, ErrorPolicyProvider},
+};
 
-// TODO(chase): Implement custom error type
-pub struct Error {}
+pub struct Error(TransactionDbError);
 
-// TODO(chase): Implement event handler
-pub fn on_chain_event(conn: PgConnection, ev: Event) -> Result<(), Error> {
-  todo!("Implement event handler")
+pub async fn on_chain_event(conn: &mut PgConnection, ev: Event) -> Result<(), Error> {
+  match ev.data {
+    // TODO(chase): Ignore "transaction not found" db errors.
+    EventData::Transaction(_) => conn
+      .set_tx_submitted(
+        &ev.context.tx_hash.unwrap()[..],
+        ev.context.block_number.unwrap(),
+      )
+      .await
+      .map_err(Error),
+    EventData::RollBack {
+      block_slot,
+      block_hash: _,
+    } => conn.rm_txs_after_block(block_slot).await.map_err(Error),
+    // Uninteresting events
+    _ => Ok(()),
+  }
 }
 
+// TODO(chase): Implement proper error policy
 impl ErrorPolicyProvider for Error {
   fn get_error_policy(&self) -> ErrorPolicy<Self> {
     ErrorPolicy::Skip
