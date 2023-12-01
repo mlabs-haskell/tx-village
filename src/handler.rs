e7aaab505e13b18937b21e7abd97a2ef26c270cc
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2023-11-30 19:43:08 -0700

    Utilize span based logging in handler

--- src/handler.rs
+++ src/handler.rs
@@ -1,8 +1,8 @@
-use oura::model::{Event, EventData};
+use oura::model as oura;
 
 use sqlx::PgConnection;
 use strum_macros::Display;
-use tracing::{event, span, Level};
+use tracing::{event, span, Instrument, Level};
 
 use infinity_query::{
   database::{errors::TransactionDbError, TransactionSql},
@@ -11,33 +11,46 @@ use infinity_query::{
 
 pub struct Error(TransactionDbError);
 
-pub async fn on_chain_event(conn: &mut PgConnection, ev: Event) -> Result<(), Error> {
-  let span = span!(Level::INFO, "on_chain_event");
-  let _enter = span.enter();
-  match ev.data {
-    // TODO(chase): These unwraps shouldn't fail but maybe they should still be checked.
-    // TODO(chase): Ignore "transaction not found" db errors.
-    EventData::Transaction(t) => {
-      let tx_id = &ev.context.tx_hash.unwrap();
-      event!(Level::DEBUG, task=%Events::HandlingTransactionEvent, tx_id);
-      conn
-        .save_tx(tx_id, &t.hash, ev.context.block_number.unwrap())
+pub async fn on_chain_event(conn: &mut PgConnection, ev: oura::Event) -> Result<(), Error> {
+  let span = span!(Level::INFO, "HandlingEvent", event=?ev.context);
+  async move {
+    match ev.data {
+      // TODO(chase): These unwraps shouldn't fail but maybe they should still be checked.
+      // TODO(chase): Ignore "transaction not found" db errors.
+      oura::EventData::Transaction(t) => {
+        let tx_id = &ev.context.tx_hash.unwrap();
+        let span = span!(Level::DEBUG, "HandlingTransactionEvent", tx_id);
+        async move {
+          conn
+            .save_tx(tx_id, &t.hash, ev.context.block_number.unwrap())
+            .await
+            .map_err(Error)
+        }
+        .instrument(span)
         .await
-        .map_err(Error)
-    }
-    EventData::RollBack {
-      block_slot,
-      block_hash: _,
-    } => {
-      event!(Level::DEBUG, task=%Events::HandlingRollbackEvent, block_slot);
-      conn.rollback_after_block(block_slot).await.map_err(Error)
-    }
-    // Uninteresting events
-    _ => {
-      event!(Level::DEBUG, task=%Events::SkippingEvent);
-      Ok(())
+      }
+      oura::EventData::RollBack {
+        block_slot,
+        block_hash: _,
+      } => {
+        let span = span!(Level::DEBUG, "HandlingTransactionEvent", block_slot);
+        conn
+          .rollback_after_block(block_slot)
+          .instrument(span)
+          .await
+          .map_err(|err| {
+            event!(Level::ERROR, label=%Event::RollbackError, ?err);
+            Error(err)
+          })
+      }
+      _ => {
+        event!(Level::DEBUG, label=%Event::SkippingEvent);
+        Ok(())
+      }
     }
   }
+  .instrument(span)
+  .await
 }
 
 // TODO(chase): Implement proper error policy
@@ -48,8 +61,7 @@ impl ErrorPolicyProvider for Error {
 }
 
 #[derive(Display)]
-enum Events {
-  HandlingTransactionEvent,
-  HandlingRollbackEvent,
+enum Event {
+  RollbackError,
   SkippingEvent,
 }
