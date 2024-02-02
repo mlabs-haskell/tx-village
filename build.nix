45221c15f1f63c8c6885641056e4f28143fde981
Drazen Popovic <bladyjoker@gmail.com>
2024-02-02 16:57:12 +0100

    Adds dependencies to infinity-query

--- build.nix
+++ build.nix
@@ -6,6 +6,16 @@
           src = ./.;
           inherit (inputs) crane;
           crateName = "infinity-query";
+          extraSources = [
+            {
+              name = "plutip";
+              path = config.packages.plutip-rust-src;
+            }
+            {
+              name = "infinity-transactions";
+              path = config.packages.infinity-transactions-rust-src;
+            }
+          ];
           devShellHook = config.settings.shell.hook;
           devShellTools = with inputs'; [ plutip.packages."plutip-core:exe:local-cluster" ];
           testTools = with inputs'; [ plutip.packages."plutip-core:exe:local-cluster" ];
