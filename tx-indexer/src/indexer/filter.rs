057d0db00134a646a4def88761b364efb6280cda
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2024-05-02 19:02:22 -0600

    Filter to only contain transaction events

--- tx-indexer/src/indexer/filter.rs
+++ tx-indexer/src/indexer/filter.rs
@@ -5,15 +5,20 @@ pub struct Filter {
     pub curr_symbols: Vec<String>,
 }
 
+// We only obtain Transaction events that contain the policy in the output
+// NOTE: Must enable 'include_transaction_details' in oura Mapper config.
 impl Filter {
     pub fn to_selection_config(self) -> Config {
         Config {
-            check: Predicate::AnyOf(
-                self.curr_symbols
-                    .into_iter()
-                    .map(Predicate::PolicyEquals)
-                    .collect(),
-            ),
+            check: Predicate::AllOf(vec![
+                Predicate::VariantIn(vec!["Transaction".to_string()]),
+                Predicate::AnyOf(
+                    self.curr_symbols
+                        .into_iter()
+                        .map(Predicate::PolicyEquals)
+                        .collect(),
+                ),
+            ]),
         }
     }
 }
