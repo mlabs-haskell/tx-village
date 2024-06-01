ac725a7fa2715ee3564243cb4386fcc7cddc5c3d
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2024-06-01 13:43:58 -0600

    Add user ctx, user state, and utility functions to access ledger state

--- ledger-sim/src/Ledger/Sim/Test.hs
+++ ledger-sim/src/Ledger/Sim/Test.hs
@@ -1,7 +1,16 @@
-module Ledger.Sim.Test (LedgerExpectation, ledgerTestGroup, ledgerTestCase, ledgerSucceeds, ledgerSucceedsWith, ledgerFails, ledgerFailsWith) where
+module Ledger.Sim.Test (
+    LedgerExpectation,
+    ledgerTestGroup,
+    ledgerTestCase,
+    ledgerSucceeds,
+    ledgerSucceedsWith,
+    ledgerFails,
+    ledgerFailsWith,
+) where
 
 import Control.Monad.Trans.Reader (Reader, ask, runReader)
 import Data.Either (isLeft)
+
 import Test.Tasty (TestName, TestTree, testGroup)
 import Test.Tasty.HUnit (Assertion, assertBool, testCase, (@?=))
 
@@ -13,32 +22,32 @@ import Ledger.Sim (
     runLedgerSim,
  )
 
-ledgerTestGroup :: LedgerConfig -> LedgerState -> TestName -> [Reader (LedgerConfig, LedgerState) TestTree] -> TestTree
+ledgerTestGroup :: LedgerConfig ctx -> LedgerState st -> TestName -> [Reader (LedgerConfig ctx, LedgerState st) TestTree] -> TestTree
 ledgerTestGroup cfg st testName trees = testGroup testName $ flip runReader (cfg, st) <$> trees
 
-ledgerTestCase :: TestName -> LedgerExpectation -> Reader (LedgerConfig, LedgerState) TestTree
+ledgerTestCase :: TestName -> LedgerExpectation ctx st -> Reader (LedgerConfig ctx, LedgerState st) TestTree
 ledgerTestCase testName test = do
     (cfg, st) <- ask
     pure . testCase testName $ toAssertion cfg st test
 
-ledgerSucceeds :: (Eq e, Show e) => LedgerSim e () -> LedgerExpectation
+ledgerSucceeds :: (Eq e, Show e) => LedgerSim ctx st e () -> LedgerExpectation ctx st
 ledgerSucceeds = ledgerSucceedsWith ()
 
-ledgerSucceedsWith :: (Eq e, Show e, Eq a, Show a) => a -> LedgerSim e a -> LedgerExpectation
+ledgerSucceedsWith :: (Eq e, Show e, Eq a, Show a) => a -> LedgerSim ctx st e a -> LedgerExpectation ctx st
 ledgerSucceedsWith = SucceedsWith
 
-ledgerFails :: LedgerSim e () -> LedgerExpectation
+ledgerFails :: LedgerSim ctx st e () -> LedgerExpectation ctx st
 ledgerFails = Fails
 
-ledgerFailsWith :: (Eq e, Show e, Eq a, Show a) => LedgerValidatorError e -> LedgerSim e a -> LedgerExpectation
+ledgerFailsWith :: (Eq e, Show e, Eq a, Show a) => LedgerValidatorError e -> LedgerSim ctx st e a -> LedgerExpectation ctx st
 ledgerFailsWith = FailsWith
 
-data LedgerExpectation
-    = forall e a. (Eq e, Show e, Eq a, Show a) => SucceedsWith a (LedgerSim e a)
-    | forall e a. (Eq e, Show e, Eq a, Show a) => FailsWith (LedgerValidatorError e) (LedgerSim e a)
-    | forall e a. Fails (LedgerSim e a)
+data LedgerExpectation ctx st
+    = forall e a. (Eq e, Show e, Eq a, Show a) => SucceedsWith a (LedgerSim ctx st e a)
+    | forall e a. (Eq e, Show e, Eq a, Show a) => FailsWith (LedgerValidatorError e) (LedgerSim ctx st e a)
+    | forall e a. Fails (LedgerSim ctx st e a)
 
-toAssertion :: LedgerConfig -> LedgerState -> LedgerExpectation -> Assertion
+toAssertion :: LedgerConfig ctx -> LedgerState st -> LedgerExpectation ctx st -> Assertion
 toAssertion cfg st (SucceedsWith x sim) = runLedgerSim cfg st sim @?= Right x
 toAssertion cfg st (FailsWith x sim) = runLedgerSim cfg st sim @?= Left x
 toAssertion cfg st (Fails sim) =
--- ledger-sim/src/Ledger/Sim/Types/Config.hs
+++ ledger-sim/src/Ledger/Sim/Types/Config.hs
@@ -7,17 +7,20 @@ import PlutusLedgerApi.V2 qualified as PlutusV2
 
 import Ledger.Sim.Types.CostModel (PlutusCostModel (PlutusCostModel), mkEvaluationContext)
 
-data LedgerConfig = LedgerConfig
+data LedgerConfig ctx = LedgerConfig
     { lc'evaluationContext :: Plutus.EvaluationContext
     , lc'scriptStorage :: !(Map PlutusV2.ScriptHash PlutusV2.ScriptForEvaluation)
+    , lc'userCtx :: !ctx
     }
 
 -- | A ledger config built with 'practicalSlotConfig' and the given cost model.
 mkLedgerConfig ::
     Map PlutusV2.ScriptHash PlutusV2.ScriptForEvaluation ->
     PlutusCostModel ->
-    Either PlutusV2.CostModelApplyError LedgerConfig
-mkLedgerConfig scripts costModel =
+    ctx ->
+    Either PlutusV2.CostModelApplyError (LedgerConfig ctx)
+mkLedgerConfig scripts costModel userCtx =
     LedgerConfig
         <$> mkEvaluationContext costModel
         <*> pure scripts
+        <*> pure userCtx
