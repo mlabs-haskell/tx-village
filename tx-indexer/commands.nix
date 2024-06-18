220146f42db7bbd885cfa2c38bf049cac4609dea
Szabo Gergely <gege251@mailbox.org>
2024-06-18 15:09:25 +0200

    Refactor db commands

---
+++ tx-indexer/commands.nix
@@ -0,0 +1,80 @@
+{ pkgs
+, extraDdls ? [ ]
+, pgUser ? "tx_indexer"
+, pgPort ? "5555"
+, pgDir ? "$PWD/.pg"
+, postgresql ? pkgs.postgresql_16
+}:
+let
+  postgresConf =
+    pkgs.writeText "postgresql.conf"
+      ''
+        # Add Custom Settings
+        log_min_messages = warning
+        log_min_error_statement = error
+        log_min_duration_statement = 100  # ms
+        log_connections = on
+        log_disconnections = on
+        log_duration = on
+        #log_line_prefix = '[] '
+        log_timezone = 'UTC'
+        log_statement = 'all'
+        log_directory = 'pg_log'
+        log_filename = 'postgresql-%Y-%m-%d_%H%M%S.log'
+        logging_collector = on
+        log_min_error_statement = error
+      '';
+
+  ddls = [ ./db/plutus.ddl ] ++ extraDdls;
+
+  init-db = pkgs.writeShellApplication {
+    name = "init-db";
+    runtimeInputs = [ postgresql ];
+    runtimeEnv = {
+      LC_CTYPE = "en_US.UTF-8";
+      LC_ALL = "en_US.UTF-8";
+      LANG = "en_US.UTF-8";
+    };
+    text = ''
+      export PGDATA="${pgDir}"
+      export PGHOST="${pgDir}"
+      [ ! -d "$PGDATA" ] && pg_ctl initdb -o "-U ${pgUser}" && cat "${postgresConf}" >> "$PGDATA/postgresql.conf"
+
+      start-db
+      echo "CREATE DATABASE ${pgUser}" | psql -p "${pgPort}" -U "${pgUser}" postgres
+
+    '' + pkgs.lib.concatStrings (map (ddl: "pg < ${ddl}") ddls);
+  };
+
+  start-db = pkgs.writeShellApplication {
+    name = "start-db";
+    runtimeInputs = [ postgresql ];
+    text = ''
+      export PGDATA="${pgDir}"
+      pg_ctl -o "-p ${pgPort} -k $PGDATA" start
+    '';
+  };
+
+  stop-db = pkgs.writeShellApplication {
+    name = "stop-db";
+    runtimeInputs = [ postgresql ];
+    text = ''
+      export PGDATA="${pgDir}"
+      pg_ctl stop && exit
+    '';
+  };
+
+  pg = pkgs.writeShellApplication {
+    name = "pg";
+    runtimeInputs = [ postgresql ];
+    text = ''
+      export PGHOST="${pgDir}"
+      psql -p ${pgPort} -U ${pgUser}
+    '';
+  };
+
+
+
+in
+{ inherit init-db start-db stop-db pg; }
+
