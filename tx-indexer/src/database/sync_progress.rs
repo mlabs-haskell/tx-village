53478c5c5a09122a7a37cc6be98b4ea0f627b605
Szabo Gergely <gege251@mailbox.org>
2024-08-02 21:18:59 +0200

    Keeping track of processed and block

--- tx-indexer/src/database/sync_progress.rs
+++ tx-indexer/src/database/sync_progress.rs
@@ -1,5 +1,5 @@
 use data_encoding::HEXLOWER;
-use sqlx::{FromRow, PgConnection};
+use sqlx::{Connection, FromRow, PgConnection};
 use tracing::{info_span, span, Instrument, Level};
 
 #[derive(Clone, Debug, FromRow, Eq, PartialEq)]
@@ -20,31 +20,43 @@ impl SyncProgressTable {
     pub async fn get(conn: &mut PgConnection) -> Result<Option<Self>, sqlx::Error> {
         let span = info_span!("Get SyncProgress");
         // Get existing entity
-        sqlx::query_as::<_, Self>("SELECT block_slot, block_hash FROM sync_progress")
-            .fetch_optional(conn)
-            .instrument(span)
-            .await
+        sqlx::query_as::<_, Self>(
+            "SELECT block_slot, block_hash FROM sync_progress WHERE processed = TRUE",
+        )
+        .fetch_optional(conn)
+        .instrument(span)
+        .await
     }
 
     /// Save a new entity to the database.
     pub async fn store(&self, conn: &mut PgConnection) -> Result<(), sqlx::Error> {
         let span = span!(Level::INFO, "Store SyncProgress", ?self.block_slot);
-        // Insert new entity
+        let _ = span.enter();
+        let mut txn = conn.begin().await?;
+        // Set finished block as processed
+        sqlx::query("DELETE FROM sync_progress WHERE processed = TRUE")
+            .bind(self.block_slot)
+            .bind(self.block_hash.clone())
+            .execute(&mut *txn)
+            .await?;
+        sqlx::query("UPDATE sync_progress SET processed = TRUE WHERE processed = FALSE")
+            .bind(self.block_slot)
+            .bind(self.block_hash.clone())
+            .execute(&mut *txn)
+            .await?;
+
+        // Insert new in progress block
         sqlx::query(
-            r#"
-                INSERT INTO sync_progress (block_slot, block_hash)
-                VALUES ($1, $2)
-                ON CONFLICT (id)
-                DO UPDATE SET
-                    block_slot = EXCLUDED.block_slot,
-                    block_hash = EXCLUDED.block_hash
-                "#,
+            r#"INSERT INTO sync_progress (block_slot, block_hash, processed)
+               VALUES ($1, $2, FALSE)"#,
         )
         .bind(self.block_slot)
         .bind(self.block_hash.clone())
-        .execute(conn)
+        .execute(&mut *txn)
         .instrument(span)
         .await?;
+
+        txn.commit().await?;
 
         Ok(())
     }
