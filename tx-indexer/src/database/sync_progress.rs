9009c2aa120717ab3347c59e46158691a67e794d
Szabo Gergely <gege251@mailbox.org>
2024-08-06 16:05:55 +0200

    Never override unprocessed block

--- tx-indexer/src/database/sync_progress.rs
+++ tx-indexer/src/database/sync_progress.rs
@@ -1,6 +1,6 @@
 use data_encoding::HEXLOWER;
 use sqlx::{Connection, FromRow, PgConnection};
-use tracing::{info_span, span, Instrument, Level};
+use tracing::{debug, info_span, span, Instrument, Level};
 
 #[derive(Clone, Debug, FromRow, Eq, PartialEq)]
 pub struct SyncProgressTable {
@@ -32,31 +32,40 @@ impl SyncProgressTable {
     pub async fn store(&self, conn: &mut PgConnection) -> Result<(), sqlx::Error> {
         let span = span!(Level::INFO, "Store SyncProgress", ?self.block_slot);
         let _ = span.enter();
-        let mut txn = conn.begin().await?;
-        // Set finished block as processed
-        sqlx::query("DELETE FROM sync_progress WHERE processed = TRUE")
-            .bind(self.block_slot)
-            .bind(self.block_hash.clone())
-            .execute(&mut *txn)
-            .await?;
-        sqlx::query("UPDATE sync_progress SET processed = TRUE WHERE processed = FALSE")
-            .bind(self.block_slot)
-            .bind(self.block_hash.clone())
-            .execute(&mut *txn)
-            .await?;
-
-        // Insert new in progress block
-        sqlx::query(
-            r#"INSERT INTO sync_progress (block_slot, block_hash, processed)
-               VALUES ($1, $2, FALSE)"#,
+        // Find already stored unprocessed block to avoid progressing when the same block event is
+        // emitted
+        let already_stored = sqlx::query_as::<_, Self>(
+            "SELECT block_slot, block_hash FROM sync_progress WHERE processed = FALSE AND block_slot = $1",
         )
         .bind(self.block_slot)
-        .bind(self.block_hash.clone())
-        .execute(&mut *txn)
-        .instrument(span)
+        .fetch_optional(&mut *conn)
         .await?;
 
-        txn.commit().await?;
+        if already_stored.is_none() {
+            let mut txn = conn.begin().await?;
+            // Set finished block as processed
+            sqlx::query("DELETE FROM sync_progress WHERE processed = TRUE")
+                .bind(self.block_slot)
+                .execute(&mut *txn)
+                .await?;
+            sqlx::query("UPDATE sync_progress SET processed = TRUE WHERE processed = FALSE")
+                .bind(self.block_slot)
+                .execute(&mut *txn)
+                .await?;
+
+            // Insert new in progress block
+            sqlx::query("INSERT INTO sync_progress (block_slot, block_hash, processed) VALUES ($1, $2, FALSE)")
+                .bind(self.block_slot)
+                .bind(self.block_hash.clone())
+                .execute(&mut *txn)
+                .await?;
+
+            txn.commit().await?;
+
+            debug!("Stored Sync Progress")
+        } else {
+            debug!("Duplicate Sync Progress event")
+        }
 
         Ok(())
     }
