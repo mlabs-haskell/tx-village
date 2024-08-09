19ce9ef12111afbbc95edc099d6c0364c91b42f7
Szabo Gergely <gege251@mailbox.org>
2024-08-09 14:33:12 +0200

    Add TryFrom<OgmiosOpts> for OgmiosClientConfig

--- tx-bakery-ogmios/src/clap.rs
+++ tx-bakery-ogmios/src/clap.rs
@@ -1,6 +1,9 @@
+use anyhow::anyhow;
 use clap::Parser;
 use tx_bakery::chain_query::Network;
 use url::Url;
+
+use crate::client::{OgmiosClientConfig, OgmiosClientConfigBuilder};
 
 #[derive(Debug, Clone, Parser)]
 pub struct OgmiosOpts {
@@ -11,4 +14,15 @@ pub struct OgmiosOpts {
     /// Cardano network type (mainnet | testnet|)
     #[arg(long, value_name = "NETWORK", default_value = "testnet")]
     pub network: Network,
+}
+
+impl TryFrom<OgmiosOpts> for OgmiosClientConfig {
+    type Error = anyhow::Error;
+    fn try_from(opts: OgmiosOpts) -> Result<OgmiosClientConfig, anyhow::Error> {
+        OgmiosClientConfigBuilder::default()
+            .url(opts.ogmios_url)
+            .network(opts.network)
+            .build()
+            .map_err(|err| anyhow!("Couldn't build OgmiosClientConfig: {}", err))
+    }
 }
