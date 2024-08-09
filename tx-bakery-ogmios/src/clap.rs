efda8edc71b637c415808a3e85d90a34dc61de4e
Szabo Gergely <gege251@mailbox.org>
2024-08-09 14:23:09 +0200

    Make clap Opts public

--- tx-bakery-ogmios/src/clap.rs
+++ tx-bakery-ogmios/src/clap.rs
@@ -3,7 +3,7 @@ use tx_bakery::chain_query::Network;
 use url::Url;
 
 #[derive(Debug, Clone, Parser)]
-struct OgmiosOpts {
+pub struct OgmiosOpts {
     /// URL of the Ogmios service
     #[arg(long, value_name = "URL", default_value = "http://127.0.0.1:1337")]
     pub ogmios_url: Url,
--- tx-bakery/src/clap.rs
+++ tx-bakery/src/clap.rs
@@ -3,12 +3,12 @@ use std::path::PathBuf;
 use clap::Parser;
 
 #[derive(Debug, Clone, Parser)]
-struct KeyWalletOpts {
+pub struct KeyWalletOpts {
     /// Payment signing key
     #[arg(long, value_name = "FILE")]
-    signing_key_file: PathBuf,
+    pub signing_key_file: PathBuf,
 
     /// Optional staking signing key
     #[arg(long, value_name = "FILE")]
-    staking_signing_key_file: Option<PathBuf>,
+    pub staking_signing_key_file: Option<PathBuf>,
 }
