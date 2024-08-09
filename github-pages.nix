610e60d61d63c7c37b8cbde8c6ee09cec720aa0d
Hongrui Fang <chfanghr@gmail.com>
2024-08-09 19:05:38 +0800

    dont reuse README

--- github-pages.nix
+++ github-pages.nix
@@ -4,13 +4,28 @@
   perSystem = { config, pkgs, ... }:
     let
       github-pages-main-page = pkgs.writeText "README.md" ''
-        ${builtins.readFile ./README.md}
+        # [Transaction Village](https://github.com/mlabs-haskell/tx-village)
+
+        ## Packages
+
+        - [tx-bakery](https://github.com/mlabs-haskell/tx-village/tree/main/tx-bakery) - Transaction Bakery - Rust based transaction builder
+          library
+        - [tx-bakery-ogmios](https://github.com/mlabs-haskell/tx-village/tree/main/tx-bakery-ogmios) - Ogmios Support for Transaction Bakery
+        - [tx-bakery-plutip](https://github.com/mlabs-haskell/tx-village/tree/main/tx-bakery-plutip) - Plutip Support for Transaction Bakery
+        - [tx-indexer](https://github.com/mlabs-haskell/tx-village/tree/main/tx-indexer) - Transaction Indexer - Rust based chain follower
+          and indexer
 
         ## API References
 
         - [tx-bakery](./artifacts/tx-bakery/tx_bakery/index.html)
         - [tx-bakery-ogmios](./artifacts/tx-bakery-ogmios/tx_bakery_ogmios/index.html)
         - [tx-bakery-plutip](./artifacts/tx-bakery-plutip/tx_bakery_plutip/index.html)
+        - [tx-indexer](./artifacts/tx-indexer/tx_indexer/index.html)
+
+        ## Documents
+
+        - [Contributing guideline](https://github.com/mlabs-haskell/tx-village/blob/main/CONTRIBUTING.md)
+        - [License](https://github.com/mlabs-haskell/tx-village/blob/main/LICENSE)
       '';
 
       github-pages = pkgs.stdenv.mkDerivation {
@@ -26,6 +41,7 @@
           cp -L -v -r ${config.packages.tx-bakery-rust-doc}/share/doc $out/artifacts/tx-bakery
           cp -L -v -r ${config.packages.tx-bakery-ogmios-rust-doc}/share/doc $out/artifacts/tx-bakery-ogmios
           cp -L -v -r ${config.packages.tx-bakery-plutip-rust-doc}/share/doc $out/artifacts/tx-bakery-plutip
+          cp -L -v -r ${config.packages.tx-indexer-rust-doc}/share/doc $out/artifacts/tx-indexer
         '';
       };
     in
