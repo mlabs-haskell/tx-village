ace6bbe3a03e56ef5f0231c96dd211f59d7f1d86
Hongrui Fang <chfanghr@gmail.com>
2024-08-09 19:09:25 +0800

    lower case s

--- README.md
+++ README.md
@@ -5,8 +5,8 @@
 - [ledger-sim](./ledger-sim/) - Haskell based Ledger simulator
 - [tx-bakery](./tx-bakery) - Transaction Bakery - Rust based transaction builder
   library
-- [tx-bakery-ogmios](./tx-bakery-ogmios) - Ogmios Support for Transaction Bakery
-- [tx-bakery-plutip](./tx-bakery-plutip) - Plutip Support for Transaction Bakery
+- [tx-bakery-ogmios](./tx-bakery-ogmios) - Ogmios support for Transaction Bakery
+- [tx-bakery-plutip](./tx-bakery-plutip) - Plutip support for Transaction Bakery
 - [tx-indexer](./tx-indexer/) - Transaction Indexer - Rust based chain follower
   and indexer
 
--- github-pages.nix
+++ github-pages.nix
@@ -10,8 +10,8 @@
 
         - [tx-bakery](https://github.com/mlabs-haskell/tx-village/tree/main/tx-bakery) - Transaction Bakery - Rust based transaction builder
           library
-        - [tx-bakery-ogmios](https://github.com/mlabs-haskell/tx-village/tree/main/tx-bakery-ogmios) - Ogmios Support for Transaction Bakery
-        - [tx-bakery-plutip](https://github.com/mlabs-haskell/tx-village/tree/main/tx-bakery-plutip) - Plutip Support for Transaction Bakery
+        - [tx-bakery-ogmios](https://github.com/mlabs-haskell/tx-village/tree/main/tx-bakery-ogmios) - Ogmios support for Transaction Bakery
+        - [tx-bakery-plutip](https://github.com/mlabs-haskell/tx-village/tree/main/tx-bakery-plutip) - Plutip support for Transaction Bakery
         - [tx-indexer](https://github.com/mlabs-haskell/tx-village/tree/main/tx-indexer) - Transaction Indexer - Rust based chain follower
           and indexer
 
