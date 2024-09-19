76fda33e8468cd3ac7241dca7e433c2ad6198bbd
Szabo Gergely <gege251@mailbox.org>
2024-09-19 14:00:22 +0200

    Fix tests

--- diesel-derive-pg/tests/main.rs
+++ diesel-derive-pg/tests/main.rs
@@ -5,15 +5,15 @@ struct SqlType;
 struct SqlInnerType;
 
 #[derive(Debug, PgCustomType)]
-#[diesel_derive(sql_type = SqlType)]
+#[diesel_derive_pg(sql_type = SqlType)]
 pub struct Newtype(#[diesel_derive_pg(sql_type = diesel::sql_types::Text)] pub String);
 
 #[derive(Debug, PgCustomType)]
 #[diesel_derive_pg(sql_type = SqlType)]
 pub struct Struct {
-    #[diesel_derive(sql_type  = diesel::sql_types::Text)]
+    #[diesel_derive_pg(sql_type  = diesel::sql_types::Text)]
     field_one: String,
 
-    #[diesel_derive(sql_type  = diesel::sql_types::BigInt)]
+    #[diesel_derive_pg(sql_type  = diesel::sql_types::BigInt)]
     field_two: i64,
 }
