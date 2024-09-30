e45c4fcc792cb57a7a88ff3b08faac8ff96d092b
Hongrui Fang <chfanghr@gmail.com>
2024-09-30 17:34:03 +0800

    dev-net: only run the test on x86_64-linux

--- dev-net/build.nix
+++ dev-net/build.nix
@@ -1,9 +1,10 @@
 { flake-parts-lib
 , self
 , withSystem
+, lib
 , ...
 }: {
-  perSystem = { pkgs, ... }: {
+  perSystem = { pkgs, system, ... }: lib.mkIf (system == "x86_64-linux") {
     checks.dev-net-nixos-test = pkgs.nixosTest (import ./test.nix {
       inherit flake-parts-lib self withSystem;
     });
