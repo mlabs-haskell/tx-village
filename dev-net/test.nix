d95cff37e0870b4d90a4be5bf2eb298254a6e3de
Hongrui Fang <chfanghr@gmail.com>
2024-09-30 15:56:38 +0800

    dev-net: run ogmios with the local cluster socket in test

--- dev-net/test.nix
+++ dev-net/test.nix
@@ -1,18 +1,27 @@
 { self, ... }: {
   name = "Test Cardano Dev Net";
 
-  nodes.machine = _: {
+  nodes.machine = { config, ... }: {
     imports = [
       self.inputs.cardano-nix.nixosModules.default
       self.outputs.nixosModules.dev-net
     ];
 
-    services.cardano-dev-net.enable = true;
+    services = {
+      cardano-dev-net.enable = true;
+      ogmios = {
+        enable = true;
+        nodeSocketPath = config.services.cardano-dev-net.socketPath;
+        nodeConfigPath = config.services.cardano-dev-net.dynamicConfigFile;
+      };
+    };
+
+    systemd.services.ogmios.after = [ "cardano-node-socket.service" ];
   };
 
   testScript = ''
     start_all()
 
-    machine.wait_for_unit("cardano-node-socket.service")
+    machine.wait_for_unit("ogmios.service")
   '';
 }
