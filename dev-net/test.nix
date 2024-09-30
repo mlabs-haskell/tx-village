1d6e75d4037b5b3a025144c88efa5f4f9493f8c5
Hongrui Fang <chfanghr@gmail.com>
2024-09-30 17:25:11 +0800

    dev-net: wait for ogmios to start listening

--- dev-net/test.nix
+++ dev-net/test.nix
@@ -1,7 +1,7 @@
 { self, ... }: {
   name = "Test Cardano Dev Net";
 
-  nodes.machine = { config, ... }: {
+  nodes.machine = { config, pkgs, lib, ... }: {
     imports = [
       self.inputs.cardano-nix.nixosModules.default
       self.outputs.nixosModules.dev-net
@@ -16,7 +16,28 @@
       };
     };
 
-    systemd.services.ogmios.after = [ "cardano-node-socket.service" ];
+    systemd.services.ogmios =
+
+      let
+        waitForOgmios = pkgs.writeShellScript "wait-for-ogmios" ''
+          function is-listening {
+            ${lib.getExe' pkgs.iproute2 "ss"} \
+              --no-header \
+              --numeric \
+              --listening \
+              --tcp src = ${config.services.ogmios.host} and sport = inet:${toString config.services.ogmios.port} | grep LISTEN
+          }
+
+          until is-listening; do sleep 10; done
+        '';
+      in
+      {
+        after = [ "cardano-node-socket.service" ];
+        serviceConfig.RestrictAddressFamilies = [ "AF_NETLINK" ];
+        postStart = ''
+          timeout 60 ${waitForOgmios}
+        '';
+      };
   };
 
   testScript = ''
