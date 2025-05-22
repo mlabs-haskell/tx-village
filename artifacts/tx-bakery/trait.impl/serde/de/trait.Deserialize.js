d98859b9acd79a400e6bcadc80732fa784cfb26e
Hercules CI Effects <hercules-ci[bot]@users.noreply.github.com>
2025-05-22 15:45:39 +0000

    Update gh-pages
    Store path: /nix/store/dcqz2dc1cc067pqq9a93r4p1sf8vgkg9-tx-village-github-pages

--- artifacts/tx-bakery/trait.impl/serde/de/trait.Deserialize.js
+++ artifacts/tx-bakery/trait.impl/serde/de/trait.Deserialize.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["tx_bakery",[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.217/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"enum\" href=\"tx_bakery/chain_query/enum.Network.html\" title=\"enum tx_bakery::chain_query::Network\">Network</a>"]]]]);
+    var implementors = Object.fromEntries([["tx_bakery",[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.219/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"enum\" href=\"tx_bakery/chain_query/enum.Network.html\" title=\"enum tx_bakery::chain_query::Network\">Network</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
