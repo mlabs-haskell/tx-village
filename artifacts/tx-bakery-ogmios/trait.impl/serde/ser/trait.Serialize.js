c91ea1a10e15846950736d87ce2a5abe2a4f0763
Hercules CI Effects <hercules-ci[bot]@users.noreply.github.com>
2025-06-18 15:50:09 +0000

    Update gh-pages
    Store path: /nix/store/k8x5lbx9vdlz8f1k6hyczgv0wrbg373b-tx-village-github-pages

--- artifacts/tx-bakery-ogmios/trait.impl/serde/ser/trait.Serialize.js
+++ artifacts/tx-bakery-ogmios/trait.impl/serde/ser/trait.Serialize.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["tx_bakery_ogmios",[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.217/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"tx_bakery_ogmios/error/struct.JsonRPCError.html\" title=\"struct tx_bakery_ogmios::error::JsonRPCError\">JsonRPCError</a>"]]]]);
+    var implementors = Object.fromEntries([["tx_bakery_ogmios",[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.219/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"tx_bakery_ogmios/error/struct.JsonRPCError.html\" title=\"struct tx_bakery_ogmios::error::JsonRPCError\">JsonRPCError</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
